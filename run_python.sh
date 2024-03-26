48d38f486a00a1f6c3bef58265efd8f59a1bce4a
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2024-03-27 01:13:33 +0530

    bash script

---
+++ run_python.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true; do
+    # Run your Python script
+    python main.py
+    
+    # Wait for 5 minutes
+    # echo "Waiting for 5 minutes..."
+    # sleep 300  # 300 seconds = 5 minutes
+done
+
